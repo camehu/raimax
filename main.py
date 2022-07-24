4537942d686ad3d91ab3e5bca4aefaafe054632c
camehu <camehu@gmail.com>
2022-07-24 03:03:46 -0300

    up240722

--- main.py
+++ main.py
@@ -19,14 +19,28 @@ async def root(request: Request):
 
 @app.post("/login")
 async def login(request: Request, username: str = Form(), password: str = Form()):
-   ''' mycursor = conex.mydb.cursor()
-    sql = "SELECT * FROM login WHERE colaborador = username and senha = password"
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-    if myresult == 1:
-        return templates.TemplateResponse("login.html", {"request" : request , "login": 1})
-    else:
-        return templates.TemplateResponse("login.html", {"request": request, "login": 0})'''
+   mycursor = conex.mydb.cursor()
+   mycursor.execute(f"SELECT * FROM login WHERE colaborador = '{username}' and senha = '{password}' ")
+   myresult = mycursor.fetchall()
+   for lgn in myresult:
+    try:
+      if lgn[1] == username and lgn[2] == password:
+          mycursor = conex.mydb.cursor()
+          mycursor.execute(f"SELECT * FROM login")
+          myresult = mycursor.fetchall()
+          return templates.TemplateResponse("usuarios.html", {"request": request, "users": myresult})
+    except:
+        return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/usuarios", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("usuarios.html", {"request" : request})
+
+
+@app.get("/aviso", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("aviso.html", {"request" : request})
 
 
 if __name__ == '__mail__':
