c2605316618c56d64d981cb6f1fb5723e7aa210e
camehu <camehu@gmail.com>
2022-07-26 20:57:07 -0300

    260722-2056

--- main.py
+++ main.py
@@ -17,6 +17,37 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request" : request})
 
 
+@app.get("/usuarios", response_class=HTMLResponse)
+async def usuarios(request: Request):
+        mycursor = conex.mydb.cursor()
+        mycursor.execute("SELECT * FROM login")
+        myresult = mycursor.fetchall()
+        return templates.TemplateResponse("inserir.html", {"request": request, "login": myresult})
+
+
+@app.get("/painel", response_class=HTMLResponse)
+async def painel(request: Request):
+    return templates.TemplateResponse("painel.html", {"request": request})
+
+
+@app.get("/aviso", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("aviso.html", {"request" : request})
+
+
+@app.get("/listaaviso", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("listaaviso.html", {"request" : request})
+
+
+@app.get("/listaviso", response_class=HTMLResponse)
+async def usuarios(request: Request):
+        mycursor = conex.mydb.cursor()
+        mycursor.execute("SELECT * FROM aviso")
+        myresult = mycursor.fetchall()
+        return templates.TemplateResponse("aviso.html", {"request": request, "aviso": myresult})
+
+
 @app.post("/validacpanel")
 async def login(request: Request, username: str = Form(), password: str = Form()):
     mycursor = conex.mydb.cursor()
@@ -28,50 +59,32 @@ async def login(request: Request, username: str = Form(), password: str = Form()
         return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.post("/painel", response_class=HTMLResponse)
-async def login(request: Request):
-    return templates.TemplateResponse("painel.html", {"request": request})
-
-
-@app.post("/usuarios", response_class=HTMLResponse)
-async def root(request: Request, username: str = Form(), password: str = Form(), nickname: str = Form(), tipo: str = Form()):
+@app.post("/inserir", response_class=HTMLResponse)
+async def inserir(request: Request, username: str = Form(), password: str = Form(), nickname: str = Form(), tipo: str = Form()):
     mycursor = conex.mydb.cursor()
     sql = f"INSERT INTO `login`(`colaborador`, `senha`, `nickname`, `tipo`) VALUES (%s, %s, %s, %s)"
     val = (username, password, nickname, tipo)
     mycursor.execute(sql, val)
     conex.mydb.commit()
-    return templates.TemplateResponse("usuarios.html", {"request": request, "USER": mycursor.rowcount})
-
-
-@app.get("/usuarios", response_class=HTMLResponse)
-async def root(request: Request):
-    mycursor = conex.mydb.cursor()
-    mycursor.execute("SELECT * FROM login")
-    myresult = mycursor.fetchall()
-    return templates.TemplateResponse("usuarios.html", {"request": request, "login": myresult})
+    return templates.TemplateResponse("inserir.html", {"request": request, "USER": mycursor.rowcount})
 
 
 @app.post("/editar", response_class=HTMLResponse)
-async def root(request: Request, id: str = Form(), username: str = Form(), password: str = Form(), nickname: str = Form(), tipo: str = Form()):
+async def editar(request: Request, id: str = Form(), username: str = Form(), password: str = Form(), nickname: str = Form(), tipo: str = Form()):
     mycursor = conex.mydb.cursor()
     sql = f"UPDATE `login` SET `id`='{id}',`colaborador`='{username}',`senha`='{password}',`nickname`='{nickname}',`tipo`='{tipo}' WHERE id = '{id}'"
     mycursor.execute(sql)
     conex.mydb.commit()
-    return  mycursor.rowcount
+    return templates.TemplateResponse("inserir.html", {"request": request, "editado": mycursor.rowcount})
 
 
 @app.post("/deletar", response_class=HTMLResponse)
-async def root(request: Request, id: str = Form()):
+async def deletar(request: Request, id: str = Form()):
     mycursor = conex.mydb.cursor()
     sql = f"DELETE FROM login WHERE id = '{id}'"
     mycursor.execute(sql)
     conex.mydb.commit()
-    return templates.TemplateResponse("usuarios.html", {"request": request, "deletado": mycursor.rowcount})
-
-
-@app.get("/aviso", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse("aviso.html", {"request" : request})
+    return templates.TemplateResponse("inserir.html", {"request": request, "deletado": mycursor.rowcount})
 
 
 if __name__ == '__mail__':
