7f7ceeb09264ffa24c36b893414efc2efb2c987f
camehu <camehu@gmail.com>
2022-08-03 01:01:06 -0300

    up030822

--- hash_token/token.py
+++ hash_token/token.py
@@ -1,15 +1,24 @@
-'''from jose import JWTError, jwt
+from jose import JWTError, jwt
 import datetime
 
-payload_data = {
-        "sub": "001",
-        "name": 'Carlos',
-        "nickname": "carlosa",
+
+def token(sub, nome, apelido):
+    payload_data = {
+        "sub": sub,
+        "name": nome,
+        "nickname": apelido,
         "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=60)
-    }
-my_secret = 'my_super_secret'
+       }
+    my_secret = 'my_super_secret'
+    token = jwt.encode(claims=payload_data, key=my_secret, algorithm='HS512')
+    return token
 
-token = jwt.encode(claims=payload_data, key=my_secret, algorithm='HS512')
 
+def decoToken(tokem):
+    try:
+      my_secret = 'my_super_secret'
+      TKN = jwt.decode(tokem, key= my_secret, algorithms=["HS256"])
+      return TKN
 
-print(token)'''
+    except jwt.ExpiredSignatureError:
+        print('Senha expirou')
