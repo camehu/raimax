83c801fd95af053a2a248c69f7bec76df1ae6e4c
camehu <camehu@gmail.com>
2022-08-05 00:05:33 -0300

    uo

--- conex.py
+++ conex.py
@@ -4,7 +4,7 @@ mydb = mysql.connector.connect(
   host="us-cdbr-east-06.cleardb.net",
   user="b6914c23389c6b",
   password="cd8edf81",
-  port = 3306
+  port = "3306",
   database="heroku_a7f279a8e4d980e"
 )
 
